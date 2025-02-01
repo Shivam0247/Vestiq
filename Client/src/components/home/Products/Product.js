4bbcce364b6aeafee19c3c6ebdf7956d2f6a7d10
Shivam Patel <112711878+Shivam0247@users.noreply.github.com>
2025-02-01 18:32:38 +0530

    div added

--- Client/src/components/home/Products/Product.js
+++ Client/src/components/home/Products/Product.js
@@ -72,58 +72,60 @@ const Product = (props) => {
     //     </div>
     //   </div>
     // </div>
+    <div onClick={handleProductClick}>
+      <Card
+        // isPressable
+        shadow="sm"
+        className="group w-[100%]"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onClick={(e) => {
+          if (window.innerWidth > 768) {
+            handleProductClick(e);
+          }
+        }}
+        onPress={handleProductClick}
+      >
+        <CardBody className="overflow-visible p-0">
+          <Chip
+            variant="flat"
+            className="absolute top-3 right-5 bg-gradient-to-r from-[#8f8f8f] to-[#373d49] text-white rounded-full px-3 py-1 text-sm font-medium"
+          >
+            {props.Status}
+          </Chip>
 
-    <Card
-      isPressable
-      shadow="sm"
-      className="group w-[100%]"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      onClick={(e) => {
-        if (window.innerWidth > 768) {
-          handleProductClick(e);
-        }
-      }}
-    >
-      <CardBody className="overflow-visible p-0">
-        <Chip
-          variant="flat"
-          className="absolute top-3 right-5 bg-gradient-to-r from-[#8f8f8f] to-[#373d49] text-white rounded-full px-3 py-1 text-sm font-medium"
-        >
-          {props.Status}
-        </Chip>
+          <img
+            alt={props.productName}
+            className="w-auto max-w-full max-h-full object-cover bg-transparent transition-transform duration-500 ease-in-out group-hover:scale-105 group-hover:rotate-2"
+            radius="lg"
+            shadow="sm"
+            src={hover ? props.img2 : props.img1}
+          />
+        </CardBody>
 
-        <img
-          alt={props.productName}
-          className="w-auto max-w-full max-h-full object-cover bg-transparent transition-transform duration-500 ease-in-out group-hover:scale-105 group-hover:rotate-2"
-          radius="lg"
-          shadow="sm"
-          src={hover ? props.img2 : props.img1}
-        />
-      </CardBody>
+        <CardFooter className="text-small justify-between">
+          <div className="flex flex-col items-start">
+            <b className="text-start">{props.productName}</b>
+            <p className="text-default-500">INR {props.price}</p>
+          </div>
 
-      <CardFooter className="text-small justify-between">
-        <div className="flex flex-col items-start">
-          <b className="text-start">{props.productName}</b>
-          <p className="text-default-500">INR {props.price}</p>
-        </div>
-
-        {/* Cart Icon with Hover Effect */}
-        <div
-          className="opacity-100 lg:opacity-0 lg:group-hover:opacity-100 duration-300 transform lg:group-hover:translate-y-0 lg:translate-y-3 "
-          onClick={(event) => {
-            event.stopPropagation();
-            handleAddToCart(event);
-          }}
-          onTouchStart={(event) => {
-            event.stopPropagation();
-            handleAddToCart(event);
-          }}
-        >
-          <i className="fi fi-rr-shopping-cart-add text-2xl hover:scale-110 transform duration-300 hover:text-primeColor mr-2"></i>
-        </div>
-      </CardFooter>
-    </Card>
+          {/* Cart Icon with Hover Effect */}
+          <div
+            className="opacity-100 lg:opacity-0 lg:group-hover:opacity-100 duration-300 transform lg:group-hover:translate-y-0 lg:translate-y-3 "
+            onClick={(event) => {
+              // event.stopPropagation();
+              handleAddToCart(event);
+            }}
+            onTouchStart={(event) => {
+              // event.stopPropagation();
+              handleAddToCart(event);
+            }}
+          >
+            <i className="fi fi-rr-shopping-cart-add text-2xl hover:scale-110 transform duration-300 hover:text-primeColor mr-2"></i>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
