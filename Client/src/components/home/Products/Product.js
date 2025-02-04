2a953a42f0571bd93fed0b9bc7eea51e224673ab
Shivam Patel <112711878+Shivam0247@users.noreply.github.com>
2025-02-05 00:05:25 +0530

    reload is added

--- Client/src/components/home/Products/Product.js
+++ Client/src/components/home/Products/Product.js
@@ -35,7 +35,7 @@ const Product = (props) => {
       label: props.productName,
     });
 
-    // window.location.reload();
+    window.location.reload();
   };
 
   const handleAddToCart = (event) => {
