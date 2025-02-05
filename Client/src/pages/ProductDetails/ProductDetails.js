16fa1a13eb318a4a820e9e7ecbbb3a111fbb42b0
Shivam Patel <112711878+Shivam0247@users.noreply.github.com>
2025-02-06 00:01:33 +0530

    product page is done for mobile

--- Client/src/pages/ProductDetails/ProductDetails.js
+++ Client/src/pages/ProductDetails/ProductDetails.js
@@ -388,38 +388,212 @@ const ProductDetails = () => {
         </div>
       </div>
 
-      <div className="visible-904 hidden w-full justify-center items-center">
-        <Carousel className="mx-3 mt-5">
-          <div className="flex flex-col items-center">
-            {/* Image Section */}
-            <CarouselContent>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index}>
-                  <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center">
-                        <img
-                          src="/images/image1.jpg"
-                          alt="carousel item"
-                          className="w-full"
-                        />
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-
-            <div className="mt-8 flex items-center justify-between w-full px-5">
-              <CarouselPrevious />
-              <CarouselNext />
-            </div>
-          </div>
-        </Carousel>
-      </div>
-      <div className="hidden xl:mt-8 xl:block px-10">
+      <div className="visible-904 hidden w-full flex-col mt-5">
         <div>
-          <h2 class="title font-manrope font-bold text-4xl leading-10 mb-8 text-black">
+          <h1 className="text-3xl font-semibold text-left mx-5">
+            {productInfo.name ? productInfo.name : "N/A"}
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl font-semibold text-left mx-5 mt-2">
+            INR {productInfo.price ? productInfo.price.toFixed(2) : "N/A"}
+          </h1>
+        </div>
+
+        <div>
+          <Carousel className="mx-3 mt-5">
+            <div className="flex flex-col items-center">
+              {/* Image Section */}
+              <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card>
+                        <CardContent className="flex aspect-square items-center justify-center">
+                          <img
+                            src="/images/image1.jpg"
+                            alt="carousel item"
+                            className="w-full"
+                          />
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+
+              <div className="mt-8 flex items-center justify-between w-full px-5">
+                <CarouselPrevious />
+                <CarouselNext />
+              </div>
+            </div>
+          </Carousel>
+        </div>
+
+        <div className="mt-4 mx-5">
+          <div className="flex flex-col">
+            <label className="mb-3 font-bold">Select Size</label>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button className="capitalize" variant="bordered">
+                  {selectedValue || "Select Size"}
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Size selection"
+                selectedKeys={selectedKeys}
+                selectionMode="single"
+                variant="flat"
+                onSelectionChange={setSelectedKeys}
+              >
+                {productInfo.sizes && productInfo.sizes.length > 0 ? (
+                  productInfo.sizes.map((size, index) => (
+                    <DropdownItem key={size} aria-label={`Size ${size}`}>
+                      {size}
+                    </DropdownItem>
+                  ))
+                ) : (
+                  <DropdownItem disabled>No Sizes Available</DropdownItem>
+                )}
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </div>
+
+        <div className="mt-4 mx-5">
+          <div class="relative flex items-center">
+            <button
+              type="button"
+              id="decrement-button"
+              data-input-counter-decrement="quantity-input"
+              class="bg-gray-100  hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100  focus:ring-2 focus:outline-none"
+            >
+              <svg
+                class="w-3 h-3 text-gray-900 "
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 2"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h16"
+                />
+              </svg>
+            </button>
+            <input
+              type="text"
+              id="quantity-input"
+              data-input-counter
+              aria-describedby="helper-text-explanation"
+              class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 "
+              placeholder="999"
+              required
+            />
+            <button
+              type="button"
+              id="increment-button"
+              data-input-counter-increment="quantity-input"
+              class="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none"
+            >
+              <svg
+                class="w-3 h-3 text-gray-900"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 1v16M1 9h16"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="btn mt-10 w-[100%] flex flex-col xs:flex-col sm:flex-col md:flex-row items-center justify-center">
+          <Button
+            color="primary"
+            size="lg"
+            className=" xs:w-[95%] md:w-[46%] sm:w-[95%] my-5 bg-black text-white transition-all duration-300 transform hover:scale-105 w-[95%]"
+          >
+            ADD TO CART
+          </Button>
+
+          <Button
+            color="primary"
+            size="lg"
+            className="xs:w-[95%]  w-[95%] lg:ml-0 md:w-[46%] sm:w-[95%] xs:ml-0 md:ml-5 sm:ml-0 bg-white border-black text-black border-2 transition-all duration-300 transform hover:scale-105"
+          >
+            BUY IT NOW
+          </Button>
+        </div>
+
+        <div className="desc mt-16">
+          <p className="text-center">
+            {productInfo.Description
+              ? productInfo.Description.toUpperCase()
+              : "N/A"}
+          </p>
+        </div>
+
+        <div className="FEATURES mt-10">
+          <h3 className="text-center text-lg font-extrabold mb-3">FEATURES</h3>
+          {productInfo.Features && productInfo.Features.length > 0
+            ? productInfo.Features.map((Feature, index) => (
+                <p key={index} className="text-center">
+                  {Feature.toUpperCase()}
+                </p>
+              ))
+            : null}
+        </div>
+
+        <div className="Care mt-10">
+          <h3 className="text-center text-lg font-extrabold mb-3">
+            COMPOSITION & CARE
+          </h3>
+          {productInfo.CompositionAndCare &&
+          productInfo.CompositionAndCare.length > 0
+            ? productInfo.CompositionAndCare.map((Composition_Care, index) => (
+                <p key={index} className="text-center">
+                  {Composition_Care.toUpperCase()}
+                </p>
+              ))
+            : null}
+        </div>
+
+        <div className="sizeChart mt-10 mb-10 mx-5">
+          <h3 className="text-center text-lg font-extrabold mb-5">
+            SIZE CHART
+          </h3>
+          <Table aria-label="Example table with dynamic content">
+            <TableHeader columns={columns}>
+              {(column) => (
+                <TableColumn key={column.key}>{column.label}</TableColumn>
+              )}
+            </TableHeader>
+            <TableBody items={rows}>
+              {(item) => (
+                <TableRow key={item.key}>
+                  {(columnKey) => (
+                    <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+                  )}
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+      <div className="xl:mt-8 lg:px-10">
+        <div>
+          <h2 class="title text-center font-manrope font-bold text-2xl md:text-4xl leading-10 mb-4 md:mb-8 text-black  lg:text-left">
             You may also like
           </h2>
         </div>
